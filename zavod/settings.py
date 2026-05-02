5edef929b1ab5b4efa8b3f792bbe6b4b2e57ada5
muhammadsodikruziev-max <muhammadsodikruziev@gmail.com>
2026-05-02 13:25:27 +0500

    Update settings.py

--- zavod/settings.py
+++ zavod/settings.py
@@ -58,12 +58,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'zavod.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES = {'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)}
+else:
+    DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3'}}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
