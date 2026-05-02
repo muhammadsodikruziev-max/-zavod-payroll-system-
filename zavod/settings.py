7e7e1466fb03d451589c3cfa030340a70df73cd5
muhammadsodikruziev-max <muhammadsodikruziev@gmail.com>
2026-05-02 13:47:37 +0500

    Update settings.py

--- zavod/settings.py
+++ zavod/settings.py
@@ -58,7 +58,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'zavod.wsgi.application'
 
+import os
 import dj_database_url
+
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
     DATABASES = {'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)}
